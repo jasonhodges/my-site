f464b94c9c1aae9467a6e1a34b26bedfa3468a0a
Jason Hodges <jasonlhodges@gmail.com>
2024-01-30 20:42:48 -0500

    add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import { inject } from '@vercel/analytics';
+
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+inject();
 
 bootstrapApplication(AppComponent, appConfig);
